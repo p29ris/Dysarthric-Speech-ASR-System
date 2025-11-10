d20f967f8d81da65eb5d4673d9ee360eb41b4e59
p29ris <parisburoko29@gmail.com>
2025-11-10 20:43:54 +0300

    Add: configured firebase

--- firebaseConfig.jsx
+++ firebaseConfig.jsx
@@ -3,19 +3,21 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage'; // <--- NEW IMPORT
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAk8Iho_vtSkggXjA1AQt8_MOx4z-K_0pM",
-  authDomain: "asr-system-7744a.firebaseapp.com",
-  projectId: "asr-system-7744a",
-  storageBucket: "asr-system-7744a.firebasestorage.app",
-  messagingSenderId: "448438456592",
-  appId: "1:448438456592:web:a49f4f80b227ef9e2bd595",
-  measurementId: "G-K7408M5QC2"
+    apiKey: "AIzaSyAk8Iho_vtSkggXjA1AQt8_MOx4z-K_0pM",
+    authDomain: "asr-system-7744a.firebaseapp.com",
+    projectId: "asr-system-7744a",
+    storageBucket: "asr-system-7744a.firebasestorage.app",
+    messagingSenderId: "448438456592",
+    appId: "1:448438456592:web:a49f4f80b227ef9e2bd595",
+    measurementId: "G-K7408M5QC2"
 };
 
 // Initialize Firebase
@@ -23,5 +25,6 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app); // <--- INITIALIZE STORAGE
 
-export { auth, db };
+export { auth, db, storage }; // <--- EXPORT STORAGE
