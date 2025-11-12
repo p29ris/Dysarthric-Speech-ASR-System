e9c26fb751c3d4c728783f1928a64e971b19e7c9
p29ris <parisburoko29@gmail.com>
2025-11-12 18:55:59 +0300

    Add: added firestore

--- firebaseConfig.jsx
+++ firebaseConfig.jsx
@@ -3,13 +3,9 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage'; // <--- NEW IMPORT
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getStorage } from 'firebase/storage'; 
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyAk8Iho_vtSkggXjA1AQt8_MOx4z-K_0pM",
     authDomain: "asr-system-7744a.firebaseapp.com",
@@ -25,6 +21,7 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
-const storage = getStorage(app); // <--- INITIALIZE STORAGE
+const storage = getStorage(app); 
 
-export { auth, db, storage }; // <--- EXPORT STORAGE
+// Export the initialized services
+export { auth, db, storage };
